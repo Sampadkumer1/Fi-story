0af068d7060456b8715a44c23d9ff678516dbee8
Sampadkumer1 <sampadmridha1@gmail.com>
2024-10-01 08:54:04 +0600

    Website Start

---
+++ java script.js
@@ -0,0 +1,15 @@
+// document.querySelectorAll(".imagecontener")
+// .forEach(function(elem){
+//     gsap.to(elem,{
+//         ease:Expo.easeInOut,
+//         width:'100%',
+//         duration:2,
+//         stagger:2
+//     })
+// })
+gsap.to('.imagecontener',{
+    ease:Expo.easeInOut,
+    width:'100%',
+    duration:2,
+    stagger:2
+})
